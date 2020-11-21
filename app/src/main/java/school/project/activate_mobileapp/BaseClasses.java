cbc79b94c1c55103630ce353b4017362a0da7368
dorkkelsson <mikko.jokipelto@gmail.com>
2020-11-21 15:02:11 +0200

    HelperClasses rakenne poistettu. BaseClasses tehty skeeman mukaiseksi

--- app/src/main/java/school/project/activate_mobileapp/BaseClasses.java
+++ app/src/main/java/school/project/activate_mobileapp/BaseClasses.java
@@ -1,39 +1,88 @@
 package school.project.activate_mobileapp;
-import java.util.Map;
+import android.app.Activity;
 
-import school.project.activate_mobileapp.HelperClasses;
+import java.util.ArrayList;
 
-class BaseClasses extends HelperClasses{
+class BaseClasses {
+
+    class Activities {
+        private ArrayList<Activity> activities;
+
+
+        /*GetActivities (Activities) {
+        Tähän Joona vähän tietokantataikoja joilla palautetaan lista kaikista aktiviteeteista
+        return activities
+        }
+         */
+    }
 
     class Activity {
         private String name;
         private ContactInformation contactInformation;
         private String description; //Tästä voisi tehdä apuluokan
+        private String imageURL;
         private int isAvailable; //0 = varattu/ei vapaa, 1 = vapaa
         private double price;
-        private ActivityType activityType; //numeerinen arvo aktiviteetin tyypille
+        private int activityTypeEnum; //numeerinen arvo aktiviteetin tyypille asetettava if -checkillä koodissa
+        private String activityType;
+        private AvailableTimes availableTimes;
         private String ActivityID; //tunniste tietokantaa varten
 
-        public String createID() {
-            return name;
+        public Activity(String activityID, String name, String imageURL, int activityTypeEnum, int isAvailable, double price ){
+            this.ActivityID = activityID;
+            this.name = name;
+            this.imageURL = imageURL;
+            this.activityTypeEnum = activityTypeEnum;
+            this.isAvailable = isAvailable;
+            this.price = price;
+        }
+
+        public String GetActivityType(int activityTypeEnum){
+            if (activityTypeEnum == 0){
+                return "Hippa";
+            }
+            if (activityTypeEnum == 1){
+                return "Urheilu";
+            }
+            if (activityTypeEnum == 2){
+                return "Elämys";
+            }
+            if (activityTypeEnum == 3){
+                return "Hyvinvointi";
+            }
+            return "undefined";
         }
     }
 
-    class Business {
-        private String name;
-        private ContactInformation contactInformation;
+    class AvailableTimes {
+        private ArrayList<Time> times;
 
-        private String businessID; //tunniste tietokantaa varten
+        /* Joona tänne tarvisi vähän taikoja kanssa, että saadaan kannasta haettua lista ajoista eli Time olioista:
+        public ArrayList<Time> getReservations(String ActivityID){
+        Haetaan ID:n mukaisen aktiviteetin aika -oliot.
 
-
-        public Business(String name) {
-            this.name = name;
+            return ArrayList<Time>;
         }
+         */
+    }
+    class Time {
+        private String availableTime;
+        private String date;
+        private String interval;
+        private int intervalStartTime;
+        private int intervalEndTime;
 
-        public String createID() {
-            return name;
+        public Time (String date, String interval){
+            this.date = date;
+            this.interval = interval;
+            // Tässä splittaan aikavälin listaan, jotta saamme integer tyyppiset arvot alkuajalle ja loppuajalle
+            String[] tempList = interval.split(":");
+            this.intervalStartTime = Integer.parseInt(tempList[0]);
+            this.intervalEndTime = Integer.parseInt(tempList[tempList.length - 1]);
+            //Voi olla, että tämä joudutaan vielä muuttamaan integeriksi:
+            this.availableTime = "1"; //Oletuksena asetettu aika on vapaa/olemassa
+
         }
-
     }
 
     class Customer {
@@ -50,6 +99,27 @@ class BaseClasses extends HelperClasses{
             return name;
         }
 
+    }
+
+    class ContactInformation {
+        private String phoneNumber;
+        private String email;
+
+        public String getPhoneNumber() {
+            return phoneNumber;
+        }
+
+        public void setPhoneNumber(String phoneNumber) {
+            this.phoneNumber = phoneNumber;
+        }
+
+        public String getEmail() {
+            return email;
+        }
+
+        public void setEmail(String email) {
+            this.email = email;
+        }
     }
 
 
