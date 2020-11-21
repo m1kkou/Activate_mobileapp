dfe899728636b182f50fbab9b32d94ae904f1007
Iiro Moilanen <t9moii00@students.oamk.fi>
2020-11-21 18:12:13 +0200

    Update2(luokat ja layoutit-> englanniksi + clikkaukset)

---
+++ app/src/main/java/school/project/activate_mobileapp/OrderView.java
@@ -0,0 +1,32 @@
+package school.project.activate_mobileapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class OrderView extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.orderview);
+
+        findViewById(R.id.button6).setOnClickListener(this);
+    }
+
+    public void onClick(View view) {
+        Button button;
+
+        if (view instanceof Button) {
+            button = (Button) view;
+            if (button.getId() == R.id.button6) {
+                Intent intent = new Intent(this, MainActivity.class);
+                startActivity(intent);
+            }
+
+        }
+    }
+}
