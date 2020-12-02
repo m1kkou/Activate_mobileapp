f692988718d36ec1e12bdce6e1db67271372ef32
joona.martinkauppi@gmail.com <joona.martinkauppi@gmail.com>
2020-12-02 20:39:16 +0200

    Services-luokan metodit kommentoitu

--- app/src/main/java/school/project/activate_mobileapp/Services.java
+++ app/src/main/java/school/project/activate_mobileapp/Services.java
@@ -13,6 +13,10 @@ public class Services extends MainActivity{
     public static DataSnapshot dss = MainActivity.dataSnapshot;
 
     public static void getData(DataSnapshot datasnapshot){
+        // getData saves all activity-nodes from datasnapshot to ArrayList "activitylist"
+        // Value of each activity-node from firebase is saved as Activity.class
+        // Keys of activity-nodes are saved in variable "ActivityID" to the corresponding activity.class
+        // To access AvailableTimes or Time -nodes in a specific activity-node, use getActivityTimes instead of activitylist
         Log.d("test", "getData called");
         activitylist.clear();
         DataSnapshot activitysnapshot = datasnapshot.child("Activities");
@@ -28,14 +32,20 @@ public class Services extends MainActivity{
         Log.d("test", activity0);
     }
     public static ArrayList<BaseClasses.Activity> getActivityDate(String Date){
+        // getActivityDate (parempi nimi tälle xD) goes through every activity-node in datasnapshot
+        // and saves activities in corresponding date to ArrayList DateActivityList (tälle parempi nimi kans)
+        // To get access to times in each activity, you need to use variable ActivityID with method getActivityTimes
         DataSnapshot ActivitiesSnap = dss.child("Activities");
         ArrayList<BaseClasses.Activity> DateActivityList = new ArrayList<>();
+        int Index = 0;
         for(DataSnapshot ds : ActivitiesSnap.getChildren()){
             DataSnapshot Availableds = ds.child("AvailableTimes");
             for(DataSnapshot timedss : Availableds.getChildren()){
                 String Date_ = timedss.child("Date").getValue().toString();
                 if(Date.equals(Date_)){
                     DateActivityList.add(ds.getValue(BaseClasses.Activity.class));
+                    DateActivityList.get(Index).setActivityID(ds.getKey());
+                    Index++;
                     break;
                 }
             }
@@ -44,6 +54,9 @@ public class Services extends MainActivity{
     }
 
     public static ArrayList<String> getActivityTimes(String ActivityID){
+        //getActivityTimes takes ActivityID as parameter, and uses it to find corresponding activity from DataSnapshot
+        //ActivityID is the same as key of each activity-node in firebase database
+        //Within given Activity, getActivityTimes loops through every Time-node, and saves Interval-values in ArrayList freeTimes
         ArrayList<String> freeTimes = new ArrayList();
         DataSnapshot Actds = dss.child("Activities");
         DataSnapshot ActivityDs = Actds.child(ActivityID);
--- app/src/main/java/school/project/activate_mobileapp/testi_joona.java
+++
@@ -1,6 +0,0 @@
-package school.project.activate_mobileapp;
-
-public class testi_joona {
-    //testingg
-    //testing again
-}
