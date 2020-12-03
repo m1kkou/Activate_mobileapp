d05a4ea81f2965dd482458a9ac6632545d51affa
Iiro Moilanen <t9moii00@students.oamk.fi>
2020-12-03 18:34:48 +0200

    searchmenu muutoksia

--- app/src/main/java/school/project/activate_mobileapp/SearchMenu.java
+++ app/src/main/java/school/project/activate_mobileapp/SearchMenu.java
@@ -1,66 +1,75 @@
 package school.project.activate_mobileapp;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 import java.util.ArrayList;
 
-public class SearchMenu extends AppCompatActivity implements View.OnClickListener {
+public class SearchMenu extends AppCompatActivity{
+
+    CheckBox checkBox3, checkBox8, checkBox7, checkBox9, checkBox11; //liikunta3, Hyvinvointi8, Kulttuuri7, Elamys9, Muu11
+    public static DataSnapshot dss = MainActivity.dataSnapshot;
+    public static DatabaseReference databaseReference;
+    public static DataSnapshot dataSnapshot;
+    public static ArrayList<BaseClasses.Activity> activitylist = new ArrayList<>();
+    Button button;
 
     ArrayList<BaseClasses.Activity> listWithAllActivities;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.searchmenu);
+        databaseReference = FirebaseDatabase.getInstance().getReference();
+        checkBox3 = findViewById(R.id.checkBox3); //liikunta
+        checkBox8 = findViewById(R.id.checkBox8); //Hyvinvointi
+        checkBox7 = findViewById(R.id.checkBox7); //Kulttuuri
+        checkBox9 = findViewById(R.id.checkBox9); // Elamys
+        checkBox11 = findViewById(R.id.checkBox11); // MUU
+        button = findViewById(R.id.button); //HAE
+        button.setOnClickListener(new View.OnClickListener()
 
-        findViewById(R.id.button).setOnClickListener(this);
-        //findViewById(R.id.button2).setOnClickListener(this);
+        {
+            //@Override
+            public void onClick(View v) {
+                Button button = (Button)findViewById(R.id.button);
+                ArrayList<String> filteredlist = new ArrayList<>();
 
-        listWithAllActivities = BaseClasses.activities;
+                if (checkBox3.isChecked()) {
+                    filteredlist.add("Liikunta");
+                    }
+                    if (checkBox8.isChecked()) {
+                        filteredlist.add("Hyvinvointi");
+
+                    }
+                    if (checkBox7.isChecked()) {
+                        filteredlist.add("Kulttuuri");
+
+                    }
+                    if (checkBox9.isChecked()) {
+                        filteredlist.add("Elamys");
+
+                    }
+                    if (checkBox11.isChecked()) {
+                        filteredlist.add("Muu");
+
+                    }
+                }
+
+
+            });
+
+
+        };
     }
 
-    public void onClick(View view) {
-        Button button;
 
-        if (view instanceof Button) {
-            button = (Button) view;
-            if (button.getId() == R.id.button) {
-                Intent intent = new Intent(this, SearchResults.class);
-                startActivity(intent);
-            }
 
-        }
-    }
-    //IIRO:
-    // Teetkö tämän funktion. Sen pitäisi ottaa sisään suodatus arvo esim. ActivityID
-    // tai mikä tahansa merkkijono ja sitten sen pitäisi
-    // palauttaa uusi lista mikä sisältää arvot, jotka vastaavat suodattimena annettua arvoa.
-    // Kirjoitin alle vähän mallia millä voitaisiin toteuttaa suodatus ID:n mukaan.
-    // Tämän tyyppisellä ratkaisulla voisi toteuttaa suodatuksen Aktiviteettityypin mukaan.
-
-    //Tässä olisi siis tehtävä:
-    // 1. Methodi, joka suodattaa listan AktiviteettiTyypin mukaan.
-    // 2. Syntyvä lista pitäisi viedä SearchResults puolelle.
-    // 3. Tarkasta, että aktiviteettityyppi saadaan aktiviteetti oliosta. (Huom! voi olla että joudut muuttamaan sen tietokantaan)
-    // 4. Layoutin suodatusvaihtoehdot pitäisi antaa tuo String searchParam -merkkijono.
-
-    public ArrayList<BaseClasses.Activity> FilterResultByActivityID(String searchParam) {
-        ArrayList<BaseClasses.Activity> filteredList = new ArrayList<>();
-
-        //Tässä esimerkkinä haku aktiviteettiID:n mukaan:
-        for (BaseClasses.Activity a : listWithAllActivities) {
-            if (a.getActivityID().equals(searchParam)) {
-                filteredList.add(a);
-            }
-        }
-
-        return filteredList;
-    }
-
-    //Tässä vähän enemmänkin pohtimista joten oleppa yhteydessä, jos tuntuu ettei joku asia ala ratkeamaan
-    // -Mikko
-}
